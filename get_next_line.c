bbd613ca058a31c9a7314aeb00ee066f58792063
gzovkic <g.zovkic@web.de>
2024-11-13 03:35:13 +0100

    neuer code mit weniger leaks und norminette geht auch 👍

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gzovkic <gzovkic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 15:54:14 by gzovkic           #+#    #+#             */
-/*   Updated: 2024/11/11 23:13:28 by gzovkic          ###   ########.fr       */
+/*   Updated: 2024/11/13 03:32:05 by gzovkic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,66 +14,66 @@
 
 char	*get_next_line(int fd);
 char	*make_str(char *buffer);
-void	*ft_calloc(size_t count, size_t size);
-void	ft_bzero(void *s, size_t n);
-char	*ft_strjoin(char const *s1, char const *s2);
-int		ft_strlen(const char *c);
 char	*leftover_check(char *buffer);
-char	*ft_strchr(const char *s, int c);
+char	*line_return(char **container);
+int		read_count(int fd, char **container);
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
-	int			buffer_check;
-	static char	*str;
-	char 		*str2;
-	static char	*buffer_leftovers;
-	int			newline_check;
+	char		*rt_str;
+	int			byte_length;
+	static char	*container = NULL;
 
-	newline_check = 0;
-	if(buffer_leftovers)
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	byte_length = read_count(fd, &container);
+	if (byte_length < 0 && *container)
 	{
-		str = make_str(buffer_leftovers);
-		buffer_leftovers = leftover_check(buffer_leftovers);
-		if(ft_strchr(str, '\n') || ft_strchr(str, '\0'))
-			return (str);
-					if(ft_strchr(buffer_leftovers, '\0'))
-			free(buffer_leftovers);
-		str = ft_strjoin(str, buffer_leftovers);
+		free(container);
+		container = NULL;
+		return (NULL);
 	}
-	while (newline_check == 0)
-	{
-		buffer_check = read(fd, buffer, BUFFER_SIZE);
-		if(buffer_check < 0)
-			return(NULL);
-		if(buffer_check == 0)
-		{
-			if(str == NULL || str[0] == '\0')
-			{
-				free(str);
-				return(NULL);
-			}
-			newline_check = 1;
-		}
-		buffer[buffer_check] = '\0';
-		if (str == NULL)
-			str = make_str(buffer);
-		else
-		{
-			str2 = ft_strjoin(str, buffer);
-			str = str2;
-		}
-		if(ft_strchr(str, '\n'))
-			newline_check = 1;
-	}	
-	buffer_leftovers = leftover_check(buffer);
-	buffer_leftovers[ft_strlen(buffer_leftovers)] = '\0';
-	// if(ft_strlen(buffer_leftovers) < 0)
-	// 	free(buffer_leftovers);
-	// if (*buffer_leftovers == '\0')
-	// 	free(buffer_leftovers);
-	return (str);
+	if (byte_length == 0 && !*container)
+		return (NULL);
+	rt_str = line_return(&container);
+	return (rt_str);
 }
+
+char	*line_return(char **container)
+{
+	char	*rt_str;
+
+	rt_str = make_str(*container);
+	*container = leftover_check(*container);
+	return (rt_str);
+}
+
+int	read_count(int fd, char **container)
+{
+	char	buffer[BUFFER_SIZE + 1];
+	char	*str2;
+	int		buffer_check;
+
+	if (!*container)
+		*container = ft_calloc(1, sizeof(char));
+	buffer_check = read(fd, buffer, BUFFER_SIZE);
+	if (buffer_check < 0)
+		return (0);
+	while (buffer_check > 0)
+	{
+		buffer[buffer_check] = '\0';
+		str2 = ft_strjoin(*container, buffer);
+		if (!str2)
+			return (-1);
+		free(*container);
+		*container = str2;
+		if (ft_strchr(*container, '\n'))
+			break ;
+		buffer_check = read(fd, buffer, BUFFER_SIZE);
+	}
+	return (buffer_check);
+}
+
 char	*make_str(char *buffer)
 {
 	char			*str;
@@ -84,7 +84,10 @@ char	*make_str(char *buffer)
 	count2 = 0;
 	while (buffer[count] != '\n' && buffer[count] != '\0')
 		count++;
-	str = ft_calloc(count + 1, sizeof(char));
+	if (buffer[count] == '\n')
+		str = ft_calloc(count + 2, sizeof(char));
+	else
+		str = ft_calloc(count + 1, sizeof(char));
 	if (!str)
 		return (NULL);
 	while (count > count2)
@@ -92,7 +95,7 @@ char	*make_str(char *buffer)
 		str[count2] = buffer[count2];
 		count2++;
 	}
-	if(buffer[count2] == '\n')
+	if (buffer[count2] == '\n')
 		str[count2] = '\n';
 	return (str);
 }
@@ -107,8 +110,8 @@ char	*leftover_check(char *buffer)
 	count2 = 0;
 	while (buffer[count] != '\n' && buffer[count] != '\0')
 		count++;
-	if(buffer[count] == '\0')
-		return(NULL);
+	if (buffer[count + 1] == '\0')
+		return (NULL);
 	str = ft_calloc(ft_strlen(buffer) - count + 1, sizeof(char));
 	if (!str)
 		return (NULL);
@@ -122,109 +125,29 @@ char	*leftover_check(char *buffer)
 	return (str);
 }
 
-void	*ft_calloc(size_t count, size_t size)
-{
-	size_t	num;
-	void	*ptr;
+// int	main(void)
+// {
+// 	char *new_line;
+// 	int fd;
 
-	num = 0;
-	ptr = malloc(count * size);
-	if (ptr == NULL)
-		return (NULL);
-	while (num < count * size)
-	{
-		ft_bzero(&ptr[num], size);
-		num = num + size;
-	}
-	return (ptr);
-}
-void	ft_bzero(void *s, size_t n)
-{
-	size_t			count;
-	unsigned char	*loc;
+// 	fd = open("test.txt", O_RDONLY);
+// 	if (fd < 0)
+// 		printf("ERROR OPENING FILE");
+// 	while (1)
+// 	{
+// 		new_line = get_next_line(fd);
+// 		if (!new_line)
+// 			break ;
+// 		printf("%s", new_line);
+// 		free(new_line);
+// 	}
+// // 	// new_line = get_next_line(fd);
+// // 	// printf("%s", new_line);
+// // 	// printf("%s", get_next_line(fd));
+// // 	// printf("%s", get_next_line(fd));
+// // 	// printf("%s", get_next_line(fd));
+// // 	// printf("%s", get_next_line(fd));
+// // 	// printf("%s", get_next_line(fd));
 
-	loc = (unsigned char *)s;
-	count = 0;
-	while (count < n)
-	{
-		loc[count] = 0;
-		count++;
-	}
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	int		count;
-	int		count2;
-	int		sumstr;
-	char	*newstr;
-
-	count = 0;
-	count2 = 0;
-	sumstr = ft_strlen(s1) + ft_strlen(s2);
-	newstr = ft_calloc(sumstr + 1, sizeof(char));
-	if (!newstr)
-		return (NULL);
-	while (count + count2 < sumstr)
-	{
-		if (count < ft_strlen(s1))
-		{
-			newstr[count] = s1[count];
-			count++;
-		}
-		else if (count2 < ft_strlen(s2))
-		{
-			newstr[count + count2] = s2[count2];
-			count2++;
-		}
-	}
-	return (newstr);
-}
-int	ft_strlen(const char *c)
-{
-	int	count;
-
-	count = 0;
-	while (c[count] != '\0')
-		count++;
-	return (count);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	int				count;
-	unsigned char	c2;
-
-	c2 = (unsigned char)c;
-	count = 0;
-	while (s[count] != '\0')
-	{
-		if (s[count] == c2)
-			return ((char *)&s[count]);
-		count++;
-	}
-	if (c2 == '\0')
-		return ((char *)&s[count]);
-	return (NULL);
-}
-
-int	main(void)
-{
-	char *new_line;
-	int fd;
-
-	fd = open("test.txt", O_RDONLY);
-	if (fd < 0)
-		printf("ERROR OPENING FILE");
-	while (1)
-	{
-		new_line = get_next_line(fd);
-		if (!new_line)
-			break ;
-		printf("%s", new_line);
-		free(new_line);
-	}
-	// new_line = get_next_line(fd);
-	// printf("%s", new_line);
-	return(0);
-	}
+// 	return(0);
+// 	}
--- get_next_line.h
+++ get_next_line.h
@@ -6,23 +6,28 @@
 /*   By: gzovkic <gzovkic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 10:11:13 by gzovkic           #+#    #+#             */
-/*   Updated: 2024/11/09 13:19:58 by gzovkic          ###   ########.fr       */
+/*   Updated: 2024/11/13 03:08:12 by gzovkic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
 
-#endif
+# endif
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
+void	*ft_calloc(size_t count, size_t size);
+void	ft_bzero(void *s, size_t n);
+char	*ft_strjoin(char const *s1, char const *s2);
+int		ft_strlen(const char *c);
+char	*ft_strchr(const char *s, int c);
 
 #endif
